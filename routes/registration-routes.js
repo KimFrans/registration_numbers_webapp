fa114a61613c5407225f994713cb1f69e417f5df
Kim Frans <kimfrans34@gmail.com>
2021-10-19 16:47:14 +0200

    routes changes

--- routes/registration-routes.js
+++ routes/registration-routes.js
@@ -13,18 +13,18 @@ module.exports = function registrationRoutes(registrationName) {
     }
 
     async function getReg(req, res) {
-        const databaseReg = req.body.reg;
+        // const databaseReg = req.body.reg;
         try {
+            const databaseReg = req.body.reg;
+            if (databaseReg != "") {
 
-            if (req.body.reg != "") {
-
-                if (req.body.reg != await registrationName.getDBreg()) {
+                if (databaseReg != await registrationName.getDBreg()) {
 
                     if (/[A-Z]{2}\s[0-9]{3}\-[0-9]{3}/g.test(databaseReg) || /[A-Z]{2}\s[0-9]{5}/g.test(databaseReg) || /[A-Z]{2}\-[0-9]{3}\-[0-9]{3}/g.test(databaseReg)) {
                         if (databaseReg.length > 8 && databaseReg.length <= 10) {
 
                             await registrationName.poolNameIn(databaseReg);
-
+                            
                             req.flash('Mess', 'Your registration number has been added ');
 
                             res.render('index', {
@@ -45,7 +45,7 @@ module.exports = function registrationRoutes(registrationName) {
 
                     }
                     else if (!/[A-Z]{2}\s[0-9]{3}\-[0-9]{3}/g.test(databaseReg) || !/[A-Z]{2}\s[0-9]{5}/g.test(databaseReg) || !/[A-Z]{2}\-[0-9]{3}\-[0-9]{3}/g.test(databaseReg)) {
-
+                        
                         req.flash('errorMess', 'does not match check the format!');
                         res.render('index', {
                             registration: await registrationName.getDBreg()
@@ -53,28 +53,28 @@ module.exports = function registrationRoutes(registrationName) {
 
                     }
 
-                }
-                else if(req.body.reg != undefined) {
-                    req.flash('errorMess', 'This registration already exists');
-                    res.render('index', {
-                        registration: await registrationName.getDBreg()
-                    });
-                }
+                }    
+                // else  {
+                //     req.flash('errorMess', 'This registration already exists');
+                //     res.render('index', {
+                //         registration: await registrationName.getDBreg()
+                //     });
+                // }
 
             }
-            // if (req.body.reg === await registrationName.getDBreg()) {
-            //     req.flash('errorMess', 'This registration already exists');
-            //     res.render('index', {
-            //         registration: await registrationName.getDBreg()
-            //     });
-            // }
+            if(databaseReg === await registrationName.getDBreg())  {
+                req.flash('errorMess', 'This registration already exists');
+                res.render('index', {
+                    registration: await registrationName.getDBreg()
+                });
+            }
 
-            else if (req.body.reg == "") {
+            else if (databaseReg == "") {
                 req.flash('errorMess', 'Please enter a registration number');
-
+             
                 res.render('index', {
                     registration: await registrationName.getDBreg(),
-
+                    
                 })
 
             }
@@ -103,21 +103,13 @@ module.exports = function registrationRoutes(registrationName) {
         try {
             const radioBtn = req.body.town;
             // console.log(radioBtn);
-            if (!radioBtn == undefined) {
-                const regies = await registrationName.filterRegistration(radioBtn)
-                console.log(regies);
-                res.render('index', {
-                    regies
 
-                })
-            }
-            else {
-                req.flash('errorMess', 'Please select a town');
-                res.render('index', {
-                    registration: await registrationName.getDBreg(),
-                })
-            }
+            const regies = await registrationName.filterRegistration(radioBtn)
+            console.log(regies);
+            res.render('index', {
+                regies
 
+            })
         }
         catch (err) {
             console.log(err);
